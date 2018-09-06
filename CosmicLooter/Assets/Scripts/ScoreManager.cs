0f2eabb6318dd6d9d44e7b148b83ac3a6b509b0b
DillonHandyside <dj_handyside@hotmail.com>
2018-09-06 12:09:44 +1000

    Added UI and ScoreManager scripts. Added GUI canvas to project hierarchy

---
+++ CosmicLooter/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public long score;
+    public long hiScore;
+    public int lives;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        ResetScore();
+        ResetLives();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (lives <= 0) { }
+            //gameover;
+
+        if (score > hiScore)
+            hiScore = score;
+
+        DebugKeys();
+	}
+
+    /// <summary>
+    /// Decrements the player's lives
+    /// </summary>
+    void LoseLife()
+    {
+        lives--;
+
+        if (lives < 0)
+            lives = 0;
+    }
+
+    /// <summary>
+    /// add the specified value to the player's score
+    /// </summary>
+    /// <param name="value">the amount of points to add</param>
+    void AddScore(long value)
+    {
+        score += value;
+    }
+
+    /// <summary>
+    /// sets the player's score to zero
+    /// </summary>
+    void ResetScore()
+    {
+        score = 0;
+    }
+
+    /// <summary>
+    /// sets the player's current lives to three
+    /// </summary>
+    void ResetLives()
+    {
+        lives = 3;
+    }
+
+    void DebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+        {
+            AddScore(500);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            LoseLife();
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            ResetScore();
+        }
+    }
+}
---
+++ CosmicLooter/Assets/Scripts/UI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+
+    public Text score;
+    public Text hiScore;
+    public Image[] lives;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        score.text = scoreManager.score.ToString();
+        hiScore.text = scoreManager.hiScore.ToString();
+
+        int i = 0;
+
+        foreach (Image life in lives)
+        {
+            if (i < scoreManager.lives)
+                life.enabled = true;
+            else
+                life.enabled = false;
+            
+            i++;
+        }
+    }
+}
